eb76abe4657f332dd8b5fc998dc3936f75c96341
josuelandero13 <josue.landero130899@gmail.com>
2024-05-10 21:49:38 -0600

    Query Object Phattern

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,19 +3,9 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.order(name: :asc).load_async
-    @products = Product.with_attached_photo
-    @products = @products.where(category_id: params[:category_id]) if params[:category_id]
-    @products = @products.where('price >= ?', params[:min_price]) if params[:min_price].present?
-    @products = @products.where('price <= ?', params[:max_price]) if params[:max_price].present?
-    @products = @products.search_full_text(params[:query_text]) if params[:query_text].present?
-
-    order_by = Product::ORDER_BY.fetch(
-      params[:order_by]&.to_sym, Product::ORDER_BY[:newest]
+    @pagy, @products = pagy_countless(
+      FindProduct.new.call(params), items: 12
     )
-
-    @products = @products.order(order_by)
-
-    @pagy, @products = pagy_countless(@products, items: 12)
   end
 
   def show
