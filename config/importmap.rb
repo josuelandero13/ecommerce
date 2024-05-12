e32e48b91f285580c9dc398c479047a5c73d50ff
josuelandero13 <josue.landero130899@gmail.com>
2024-05-12 17:23:04 -0600

    Adding tailwindcss-stimulus-components with importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,9 @@
-# Pin npm packages by running ./bin/importmap
+# frozen_string_literal: true
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+# Pin npm packages by running ./bin/importmap
+pin 'application'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
+pin 'tailwindcss-stimulus-components' # @5.1.1
